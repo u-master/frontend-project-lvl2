20e4972113a7598bfcecd05ac38874950cadf117
u-master <to-umaster@mail.ru>
2020-03-18 15:23:16 +0600

    modify arguments string

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,8 +1,18 @@
+
 import program from 'commander';
 
 export default () => {
   program.version('1.0.1')
     .description('Compares two configuration files and shows a difference.');
+
+  program.arguments('<firstConfig> <secondConfig>')
+    .action(
+      (firstConfig, secondConfig) => console.log(`First: ${firstConfig}, Second: ${secondConfig}`),
+    );
+
+  program.option('-f, --format [type]', 'output format');
+
   program.parse(process.argv);
-  console.log(program);
+
+  console.log(`Format: ${program.format}`);
 };
