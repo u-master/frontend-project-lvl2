e8c00f974f1e55b8737eea4a56eda1a7bb4b6c21
u-master <yurgan@gmail.com>
2020-03-26 13:39:12 +0600

    simplify difftree

--- src/difftree.js
+++ src/difftree.js
@@ -1,30 +1,27 @@
 import _ from 'lodash';
 
-const diffTree = (obj1, obj2) => {
-  const iterBuildTree = (before, after) => {
-    const getKeyState = (key) => {
-      if (!_.has(before, key)) return 'added';
-      if (!_.has(after, key)) return 'removed';
-      if (_.isObject(before[key]) && _.isObject(after[key])) return 'nested';
-      if (before[key] === after[key]) return 'unchanged';
-      return 'changed';
-    };
-
-    return _.union(Object.keys(before), Object.keys(after)).map(
-      (key) => {
-        const state = getKeyState(key);
-        return {
-          key,
-          state,
-          value: (state === 'nested')
-            ? iterBuildTree(before[key], after[key])
-            : { before: before[key], after: after[key] },
-        };
-      },
-    );
-  };
-
-  return iterBuildTree(obj1, obj2);
+const getKeyState = (key, before, after) => {
+  if (!_.has(before, key)) return 'added';
+  if (!_.has(after, key)) return 'removed';
+  if (_.isObject(before[key]) && _.isObject(after[key])) return 'nested';
+  if (before[key] === after[key]) return 'unchanged';
+  return 'changed';
 };
+
+
+const diffTree = (before, after) => _
+  .union(Object.keys(before), Object.keys(after))
+  .map(
+    (key) => {
+      const state = getKeyState(key, before, after);
+      return {
+        key,
+        state,
+        value: (state === 'nested')
+          ? diffTree(before[key], after[key])
+          : { before: before[key], after: after[key] },
+      };
+    },
+  );
 
 export default diffTree;
