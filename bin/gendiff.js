d8f7d0c61aeeddb5373c519ed0830dac5bc2dc09
u-master <to-umaster@mail.ru>
2020-04-04 17:54:22 +0600

    move cli util logic at bin

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,23 @@
 #!/usr/bin/env node
 
-import main from '../src/gendiff-cli.js';
+import program from 'commander';
+import genDiffFromFiles from '../src/gendiff-fs.js';
 
-main();
+const validFormats = ['nested', 'plain', 'json'];
+
+program
+  .version('1.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', `output format: ${validFormats.join(', ')}`, 'nested')
+  .arguments('<firstConfig> <secondConfig>')
+  .action(
+    (firstConfig, secondConfig) => {
+      if (!validFormats.includes(program.format)) {
+        console.log(`Incorrect format option '${program.format}'. Use one of following: ${validFormats.join(', ')}.`);
+      } else {
+        console.log(genDiffFromFiles(firstConfig, secondConfig, program.format));
+      }
+    },
+  );
+
+program.parse(process.argv);
