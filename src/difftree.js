72597ae422bf98cbbeea85012f1669bcfaf9706c
u-master <to-umaster@mail.ru>
2020-04-16 17:04:25 +0600

    fix keys unite

--- src/difftree.js
+++ src/difftree.js
@@ -4,13 +4,16 @@ const getKeyState = (key, before, after) => {
   if (!_.has(before, key)) return 'added';
   if (!_.has(after, key)) return 'removed';
   if (_.isObject(before[key]) && _.isObject(after[key])) return 'nested';
-  if (`${before[key]}` === `${after[key]}`) return 'unchanged';
+  if (before[key] === after[key]) return 'unchanged';
   return 'changed';
 };
 
+const joinKeys = (firstObj, secondObj) => {
+  const [keysFirst, keysSecond] = [firstObj, secondObj].map((obj) => Object.keys(obj));
+  return [...keysFirst, ...keysSecond.filter((key) => !keysFirst.includes(key))];
+};
 
-const buildDiffTree = (before, after) => _
-  .union(Object.keys(before), Object.keys(after))
+const buildDiffTree = (before, after) => joinKeys(before, after)
   .map(
     (key) => {
       const state = getKeyState(key, before, after);
