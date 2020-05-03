9729afb298f5ec0dd79052406ad04696b116cbe9
u-master <to-umaster@mail.ru>
2020-05-03 18:45:06 +0600

    fix buildDiffTree, less cyclomatic complexity

--- src/difftree.js
+++ src/difftree.js
@@ -1,32 +1,44 @@
 import _ from 'lodash';
 
-const getKeyState = (key, before, after) => {
-  if (!_.has(before, key)) return 'added';
-  if (!_.has(after, key)) return 'removed';
-  if (_.isObject(before[key]) && _.isObject(after[key])) return 'nested';
-  if (before[key] === after[key]) return 'unchanged';
-  return 'changed';
+const getNode = (key, before, after, treeBuild) => {
+  if (!_.has(before, key)) {
+    return {
+      key,
+      state: 'added',
+      value: { before: null, after: after[key] },
+      children: null,
+    };
+  }
+  if (!_.has(after, key)) {
+    return {
+      key,
+      state: 'removed',
+      value: { before: before[key], after: null },
+      children: null,
+    };
+  }
+  if (_.isObject(before[key]) && _.isObject(after[key])) {
+    return {
+      key,
+      state: 'nested',
+      value: null,
+      children: treeBuild(before[key], after[key]),
+    };
+  }
+
+  return {
+    key,
+    state: (before[key] === after[key]) ? 'unchanged' : 'changed',
+    value: { before: before[key], after: after[key] },
+    children: null,
+  };
 };
 
 const buildDiffTree = (before, after) => _
   .union(Object.keys(before), Object.keys(after))
   .sort()
   .map(
-    (key) => {
-      const state = getKeyState(key, before, after);
-      const value = (state === 'nested')
-        ? null
-        : { before: before[key], after: after[key] };
-      const children = (state === 'nested')
-        ? buildDiffTree(before[key], after[key])
-        : null;
-      return {
-        key,
-        state,
-        value,
-        children,
-      };
-    },
+    (key) => getNode(key, before, after, buildDiffTree),
   );
 
 export default buildDiffTree;
