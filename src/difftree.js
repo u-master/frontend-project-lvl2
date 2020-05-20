6c4f97b516324aacbeffbe7b587102588ac58502
u-master <yurgan@gmail.com>
2020-05-20 13:19:57 +0600

    use state array instead multy-if

--- src/difftree.js
+++ src/difftree.js
@@ -1,30 +1,36 @@
 import _ from 'lodash';
 
+const nodeStates = [
+  {
+    check: (key, before) => !_.has(before, key),
+    create: (key, before, after) => ({ key, state: 'added', value: { after: after[key] } }),
+  },
+  {
+    check: (key, before, after) => !_.has(after, key),
+    create: (key, before) => ({ key, state: 'removed', value: { before: before[key] } }),
+  },
+  {
+    check: (key, before, after) => _.isObject(before[key]) && _.isObject(after[key]),
+    create: (key, before, after, iter) => ({ key, state: 'nested', children: iter(before[key], after[key]) }),
+  },
+  {
+    check: (key, before, after) => before[key] === after[key],
+    create: (key, before, after) => ({ key, state: 'unchanged', value: { before: before[key], after: after[key] } }),
+  },
+  {
+    check: (key, before, after) => before[key] !== after[key],
+    create: (key, before, after) => ({ key, state: 'changed', value: { before: before[key], after: after[key] } }),
+  },
+];
+
 const getNode = (key, before, after, iter) => {
-  if (!_.has(before, key)) {
-    return { key, state: 'added', value: { after: after[key] } };
-  }
-
-  if (!_.has(after, key)) {
-    return { key, state: 'removed', value: { before: before[key] } };
-  }
-
-  if (_.isObject(before[key]) && _.isObject(after[key])) {
-    return { key, state: 'nested', children: iter(before[key], after[key]) };
-  }
-
-  if (before[key] === after[key]) {
-    return { key, state: 'unchanged', value: { before: before[key], after: after[key] } };
-  }
-
-  return { key, state: 'changed', value: { before: before[key], after: after[key] } };
+  const { create } = nodeStates.find(({ check }) => check(key, before, after));
+  return create(key, before, after, iter);
 };
 
 const buildDiffTree = (before, after) => _
   .union(Object.keys(before), Object.keys(after))
   .sort()
-  .map(
-    (key) => getNode(key, before, after, buildDiffTree),
-  );
+  .map((key) => getNode(key, before, after, buildDiffTree));
 
 export default buildDiffTree;
